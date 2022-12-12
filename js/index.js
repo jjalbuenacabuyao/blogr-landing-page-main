757f9d8c0d7460831138ff9bf10d6b2506ec25cb
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-12 21:30:06 +0800

    changed the isVisible value to boolean value

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
-const navToggler = document.querySelector("[data-nav-toggle]")
-const nav = document.querySelector(".nav")
+const navToggler = document.querySelector("[data-nav-toggle]");
+const nav = document.querySelector(".nav");
 
 navToggler.addEventListener("click", () => {
-  let isVisible = nav.getAttribute("data-visible")
-  if (isVisible == "false"){
-    showNav()
-  } else {
-    hideNav()
-  }
+  let isVisible = nav.getAttribute("data-visible") === "false";
+  (isVisible) ? showNav() : hideNav();
 })
 
 const showNav = () => {
-  navToggler.setAttribute("aria-expanded", true)
-  nav.setAttribute("data-visible", true)
+  navToggler.setAttribute("aria-expanded", "true");
+  nav.setAttribute("data-visible", "true");
 }
 
 const hideNav = () => {
-  navToggler.setAttribute("aria-expanded", false)
-  nav.setAttribute("data-visible", false)
+  navToggler.setAttribute("aria-expanded", "false");
+  nav.setAttribute("data-visible", "false");
 }
